f47b8b27d98166a2284bff9c5321aa0b6cfcf546
Joe Lin <joelin@digitalriver.com>
2017-01-19 04:51:52 +0000

    * Added CORS support.

---
+++ src/main/java/io/eion/security/passkeeper/config/WebConfig.java
@@ -0,0 +1,24 @@
+package io.eion.security.passkeeper.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author <a href="joelin@digitalriver.com">Joe Lin</a>
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
